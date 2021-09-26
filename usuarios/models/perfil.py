59d85d8e74132ff7e8d66c2579945b375645725d
Matheus Kontarski Barbosa <matheuskbar@gmail.com>
2021-09-26 12:38:08 -0300

    add_mask_forms

--- usuarios/models/perfil.py
+++ usuarios/models/perfil.py
@@ -8,7 +8,7 @@ class Perfil(models.Model):
     nome_completo = models.CharField(max_length=150, null=True, verbose_name='Nome Completo', help_text='Digite seu nome completo.')
     email = models.EmailField(max_length=200, blank=True, verbose_name='E-mail', help_text='Digite seu e-mail.')
     cpf = models.CharField(max_length=14, blank=True, verbose_name='CPF', help_text='Digite seu número de CPF (opcional)')
-    telefone = models.CharField(max_length=14, null=True)
+    telefone = models.CharField(max_length=15, null=True)
     foto = StdImageField('Foto de Perfil', upload_to='imagens', variations={'thumb': (124, 124)})
     slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
